5f8b4ba01a18187f8c31b4e0a6efb8ec827d1eca
raghavaggarwal2727 <raghavaggarwal2727@gmail.com>
2025-11-05 12:21:41 +0530

    Add files via upload

---
+++ day36.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[100][100]; // Adjust size as needed
+
+    // Reading matrix elements
+    printf("Enter elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Printing the matrix
+    printf("\nMatrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
