2eb3913f5f36ee4fb294e415ad53c254043e4b66
raghavaggarwal2727 <raghavaggarwal2727@gmail.com>
2025-11-05 13:07:52 +0530

    Add files via upload

---
+++ day59.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, k;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter subarray size k: ");
+    scanf("%d", &k);
+
+    if (k > n || k <= 0) {
+        printf("Invalid subarray size.\n");
+        return 1;
+    }
+
+    // Compute sum of first window
+    int maxSum = 0, windowSum = 0;
+    for (int i = 0; i < k; i++) {
+        windowSum += arr[i];
+    }
+    maxSum = windowSum;
+
+    // Slide the window
+    for (int i = k; i < n; i++) {
+        windowSum += arr[i] - arr[i - k];
+        if (windowSum > maxSum)
+            maxSum = windowSum;
+    }
+
+    printf("Maximum sum of subarrays of size %d: %d\n", k, maxSum);
+
+    return 0;
+}
