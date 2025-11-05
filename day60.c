1ca66d19b9994ae7628535f231eb0b378475ceb3
raghavaggarwal2727 <raghavaggarwal2727@gmail.com>
2025-11-05 13:08:34 +0530

    Add files via upload

---
+++ day60.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, k;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter window size k: ");
+    scanf("%d", &k);
+
+    if (k > n || k <= 0) {
+        printf("Invalid window size.\n");
+        return 1;
+    }
+
+    printf("Maximum of each subarray: ");
+    for (int i = 0; i <= n - k; i++) {
+        int max = arr[i];
+        for (int j = i + 1; j < i + k; j++) {
+            if (arr[j] > max)
+                max = arr[j];
+        }
+        printf("%d ", max);
+    }
+
+    printf("\n");
+    return 0;
+}
